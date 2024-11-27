f3e002f141226e120922db9d96f0824b998cdf73
Anna Vad <annavvad@gmail.com>
2024-11-27 09:59:48 +0100

    Initial commit

---
+++ itemServiceAPI.Test/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace itemServiceAPI.Test;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}

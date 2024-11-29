74aeef01895f3ac1f84e26bfcec3412fc4fe5234
MikkelHv <mikkelh208@gmail.com>
2024-11-29 08:30:18 +0100

    Get all items test

--- itemServiceAPI.Test/UnitTest1.cs
+++ itemServiceAPI.Test/UnitTest1.cs
@@ -68,5 +68,47 @@ namespace UnitTestController.Tests
             //Assert.IsInstanceOf<NotFoundResult>(result);
             Assert.That(result, Is.InstanceOf<NotFoundResult>());
         }
+        
+        
+        /// GetAllItems
+        
+        /// Test på at der returneres en liste, hvis items findes 
+        [Test]
+        public async Task GetAllItems_ShouldReturnListOfItems_WhenItemsExist()
+        {
+            // Arrange
+            // Her oprettes testdata, 3 items
+            var testItems = new List<Item>
+            {
+                new Item { Id = "item_001", Title = "Test Item 1" },
+                new Item { Id = "item_002", Title = "Test Item 2" },
+                new Item { Id = "item_003", Title = "Test Item 3" }
+            };
+
+            // De 3 items bliver her oprettet i mock DB
+            _itemDbRepositoryMock.Setup(repo => repo.GetAllItems())
+                                .ReturnsAsync(testItems);
+
+            // Act
+            // Henter all items
+            var result = await _itemController.GetAllItems();
+
+            // Assert
+            var okResult = result as OkObjectResult;
+            Assert.IsNotNull(okResult);
+            Assert.IsInstanceOf<List<Item>>(okResult.Value);
+
+            var returnedItems = okResult.Value as List<Item>;
+            Assert.AreEqual(3, returnedItems.Count);
+            Assert.AreEqual("item_001", returnedItems[0].Id);
+            Assert.AreEqual("item_002", returnedItems[1].Id);
+            Assert.AreEqual("item_003", returnedItems[2].Id);
+        }
+
+        // Arrange
+
+        // Act
+
+        // Assert
     }
 }

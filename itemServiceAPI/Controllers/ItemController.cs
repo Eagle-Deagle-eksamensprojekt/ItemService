2cf3b6bb51e40e4c946f58fd5529208ed9524433
MikkelHv <mikkelh208@gmail.com>
2024-12-02 13:43:20 +0100

    Create item og logger lavet

--- itemServiceAPI/Controllers/ItemController.cs
+++ itemServiceAPI/Controllers/ItemController.cs
@@ -33,9 +33,11 @@ public class ItemController : ControllerBase
         
         if (item == null) // Hvis item ikke findes
         {
+            _logger.LogWarning("Item not found.");
             return NotFound(); // Returnerer not found
         }
         
+        _logger.LogInformation("Item found.");
         return Ok(item); // Returnerer ok med item
     }
 
@@ -48,8 +50,34 @@ public class ItemController : ControllerBase
     [HttpPost]
     public async Task<IActionResult> CreateItem(Item item)
     {
-        return null; // ikke implementeret kode endnu
+        if (item == null) // Tjek for null input
+        {
+            _logger.LogWarning("Item cannot be null.");
+            return BadRequest("Item cannot be null.");
+        }
+
+        // Conflict check
+        var itemConflict = await _iItemDbRepository.GetItemById(item.Id); // Tjekker om item allerede findes
+        if (itemConflict != null)
+        {
+            _logger.LogWarning("An item with the same ID already exists.");  
+            return Conflict("An item with the same ID already exists.");
+        }
+
+        // Create item
+        var itemSuccess = await _iItemDbRepository.CreateItem(item);
+        if (itemSuccess)
+        {
+            _logger.LogInformation("Item created successfully.");
+            return CreatedAtAction(nameof(GetItem), new { id = item.Id }, item);
+        }
+        else
+        {
+            _logger.LogError("An error occurred while creating the item.");
+            return StatusCode(500, "An error occurred while creating the item.");
+        }
     }
+
 
     [HttpPut("{id}")]
     public async Task<IActionResult> UpdateItem(string id, Item item)
